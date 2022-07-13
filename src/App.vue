3bbb1a28308f8d519721ea7a34168b2c0a5e6a39
XL_Mr <643158351@qq.com>
2022-07-13 12:11:35 +0800

    配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <template>
   <div id="app">
-    黑马头条
+    <van-nav-bar title="黑马头条"></van-nav-bar>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+    <div class="box"></div>
   </div>
 </template>
-
+<script>
+export default {
+  data () {
+    return {
+      images: [
+        'https://img01.yzcdn.cn/vant/apple-1.jpg',
+        'https://img01.yzcdn.cn/vant/apple-2.jpg'
+      ]
+    }
+  }
+}
+</script>
 <style lang="less">
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+.box {
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// font
+import './assets/fonts/iconfont.css'
+// vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// flex
+import 'amfe-flexible/index'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
